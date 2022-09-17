c2d308bf791f09016987235ceaed90dfb55d88cc
gustavosalviato <gustavosalviato8@gmail.com>
2022-09-17 16:40:01 -0300

    validate if user email already exists!

--- src/modules/user/useCases/createUser/createUserUseCase.ts
+++ src/modules/user/useCases/createUser/createUserUseCase.ts
@@ -1,9 +1,23 @@
 import { User } from "@prisma/client";
 import { prisma } from "../../../../client/client";
+import { AppError } from "../../../../errors/AppError";
 import { CreatUserDTO } from "../../dtos/createUserDTO";
 
 export class CreateUserUseCase {
     async execute({ email, name }: CreatUserDTO): Promise<User> {
+
+        const userAlreadyExists = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+
+        if (userAlreadyExists) {
+            throw new AppError('user already exists!')
+        }
+
+
         const user = await prisma.user.create({
             data: {
                 email,
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import 'express-async-errors'
+import express, { NextFunction, Request, response, Response } from "express";
 import { mainRoutes } from './routes/index'
+import { AppError } from "./errors/AppError";
 
 
 const app = express()
@@ -7,5 +9,21 @@ const app = express()
 app.use(express.json())
 
 app.use(mainRoutes)
+
+app.use(
+    (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return res.status(err.statusCode).json({
+                status: 'error',
+                message: err.message
+            })
+
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: `Internal server error - ${err.message}`
+        })
+    })
 
 app.listen(3333, () => console.log('Server is runnig in port 3333 🤖'))
