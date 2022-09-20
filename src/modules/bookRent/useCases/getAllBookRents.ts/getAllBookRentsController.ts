1cc2dd19b521225ca7462e92a4fc718f1b7567e2
gustavosalviato <gustavosalviato8@gmail.com>
2022-09-20 20:53:29 -0300

    show user and books already rented in bookrents route!

--- src/modules/bookRent/useCases/getAllBookRents.ts/getAllBookRentsController.ts
+++ src/modules/bookRent/useCases/getAllBookRents.ts/getAllBookRentsController.ts
@@ -3,7 +3,24 @@ import { prisma } from "../../../../client/client";
 
 export class getAllBookRentsController {
     async handle(req: Request, res: Response) {
-        const bookRents = await prisma.bookRent.findMany()
+        const bookRents = await prisma.bookRent.findMany({
+            include: {
+                user: {
+                    select: {
+                        name: true,
+                        email: true
+                    }
+                },
+
+                book: {
+                    select: {
+                        title: true,
+                        author: true,
+                        release_date: true
+                    }
+                },
+            }
+        })
 
         res.json(bookRents)
     }
