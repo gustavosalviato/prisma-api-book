79afa5589b6af574d1193065e973be74aacc6d57
gustavosalviato <gustavosalviato8@gmail.com>
2022-09-20 20:48:49 -0300

    include user name and email in get book

--- src/modules/book/useCases/getAllBooks/getAllBooks.ts
+++ src/modules/book/useCases/getAllBooks/getAllBooks.ts
@@ -5,7 +5,23 @@ import { prisma } from "../../../../client/client";
 export class GetAllBooks {
     async execute(): Promise<Book[]> {
 
-        const books = await prisma.book.findMany()
+        const books = await prisma.book.findMany({
+            orderBy: {
+                release_date: 'desc'
+            },
+            include: {
+                BookRent: {
+                    select: {
+                        user: {
+                            select: {
+                                name: true,
+                                email: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
 
         return books
     }
